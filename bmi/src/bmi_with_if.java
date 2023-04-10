e63537edf5b953e3a0fe1128700bd70180bfd850
venoomglobal <130368399+venoomglobal@users.noreply.github.com>
2023-04-10 09:08:20 -0400

    Update bmi_with_if.java

--- bmi/src/bmi_with_if.java
+++ bmi/src/bmi_with_if.java
@@ -1,17 +1,18 @@
 
 
 import java.util.Scanner;
+
 public class bmi_with_if {
   public static void main(String[] args) {
     Scanner input = new Scanner(System.in);
-System.out.print("Enter your weight in kilograms: ");
+    System.out.print("Enter your weight in kilograms: ");
     double weight = input.nextDouble();
-System.out.print("Enter your height in meters: ");
+    System.out.print("Enter your height in meters: ");
     double height = input.nextDouble();
-double bmi = weight / (height * height);
-System.out.println("Your BMI is " + bmi);
-  }
-if (bmi < 18.5) {
+    double bmi = weight / (height * height);
+    System.out.println("Your BMI is " + bmi);
+ 
+    if (bmi < 18.5) {
       System.out.println("You are underweight.");
     } else if (bmi < 25) {
       System.out.println("You are normal weight.");
@@ -21,6 +22,6 @@ if (bmi < 18.5) {
       System.out.println("You are obese.");
     }
 
-}
+  }
 
 }
